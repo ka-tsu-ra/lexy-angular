805e11ff131a9957bba0cbfe5fcb60d33cae76d3
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-09-29 16:00:31 +0100

    Adds function to add posts to list. Unit tests passing

--- test/mainController.spec.js
+++ test/mainController.spec.js
@@ -8,6 +8,23 @@ describe('MainController', function() {
   }));
 
   it('posts contains an array of posts', function() {
-    expect(ctrl.posts).toEqual(['post 1', 'post 2', 'post 3', 'post 4', 'post 5']);
+    expect(ctrl.posts).toEqual([
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ]);
+  });
+
+  it('can add a post to the list of posts', function() {
+    ctrl.addPost({title: 'A new post!', upvotes: 0});
+    expect(ctrl.posts).toEqual([
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4},
+      {title: 'A new post!', upvotes: 0}]);
   });
 });
