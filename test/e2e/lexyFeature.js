b05d1ae20d5e741f19bfec15f4a1c330a4b0a235
Jonathan Ng <jonngkh@hotmail.com>
2015-09-29 16:44:40 +0100

    test for adding post feature

---
+++ test/e2e/lexyFeature.js
@@ -0,0 +1,18 @@
+describe('lexy forum', function() {
+
+  var title = element(by.model('mainCtrl.title'))
+  var postButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  });
+
+  it('prints all the posts', function() {
+    title.sendKeys('A new post!');
+    postButton.click();
+
+    var posts = element.all(by.repeater('post in mainCtrl.posts'));
+
+    expect(posts.getText()).toContain('A new post!');
+  });
+});
