776fa7910761328e3fc276c48a428b6496cc09be
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-09-30 13:24:27 +0100

    Refactors posts list to start off empty and refactors tests.

--- js/mainController.js
+++ js/mainController.js
@@ -1,13 +1,7 @@
 lexy.controller('MainController', [function() {
   var self = this;
 
-  self.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  self.posts = [];
 
   self.title = "";
 
--- test/mainController.spec.js
+++ test/mainController.spec.js
@@ -7,25 +7,18 @@ describe('MainController', function() {
     ctrl = $controller('MainController');
   }));
 
-  it('posts contains an array of posts', function() {
-    expect(ctrl.posts).toEqual([
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ]);
+  it('initialises with an empty list of posts and empty new post box', function() {
+    expect(ctrl.newPost).toBeUndefined();
+    expect(ctrl.posts).toEqual([]);
   });
 
-  it('can add a post to the list of posts', function() {
-    ctrl.title = 'New post!';
-    ctrl.addPost();
-    expect(ctrl.posts).toEqual([
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4},
-      {title: 'New post!', upvotes: 0}]);
+  describe('when adding a new post', function() {
+
+    it('adds the new post to the list of posts', function() {
+      ctrl.title = 'New post!';
+      ctrl.addPost();
+      expect(ctrl.posts).toEqual([
+        {title: 'New post!', upvotes: 0}]);
+    });
   });
 });
